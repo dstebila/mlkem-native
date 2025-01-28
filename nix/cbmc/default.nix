181e07b433f1fc6ff3ca3219c82fd8db6f6313a6
Hanno Becker <beckphan@amazon.co.uk>
2025-01-28 10:12:14 +0000

    Update z3 to 4.13.4
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- nix/cbmc/default.nix
+++ nix/cbmc/default.nix
@@ -30,8 +30,8 @@ buildEnv {
       cbmc-viewer = callPackage ./cbmc-viewer.nix { }; # 3.10
 
       z3 = callPackage ./z3.nix {
-        version = "4.13.3";
-        sha256 = "sha256-odwalnF00SI+sJGHdIIv4KapFcfVVKiQ22HFhXYtSvA=";
+        version = "4.13.4";
+        sha256 = "sha256-8hWXCr6IuNVKkOegEmWooo5jkdmln9nU7wI8T882BSE";
       };
 
       inherit
